7032d7c1089e1dd0bf499500f3841921b3ec27f6
Ali Eid <popa0251@gmail.com>
2025-11-28 21:31:35 +0200

    finish the task

--- OrdersTask/Program.cs
+++ OrdersTask/Program.cs
@@ -1,4 +1,17 @@
 
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using OrdersTask.APIs.Middlewares;
+using OrdersTask.Application.Interfaces;
+using OrdersTask.Application.Services;
+using OrdersTask.Application.Validators;
+using OrdersTask.Domain.Interfaces;
+using OrdersTask.Infrastructure.Cache;
+using OrdersTask.Infrastructure.Data;
+using OrdersTask.Infrastructure.Repositories;
+using Serilog;
+using StackExchange.Redis;
+
 namespace OrdersTask
 {
     public class Program
@@ -7,6 +20,17 @@ namespace OrdersTask
         {
             var builder = WebApplication.CreateBuilder(args);
 
+
+            // Configure Serilog
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(builder.Configuration)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                .WriteTo.File("logs/ordersapi-.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            builder.Host.UseSerilog();
+
             // Add services to the container.
 
             builder.Services.AddControllers();
@@ -14,9 +38,35 @@ namespace OrdersTask
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            // Database
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // redis
+            builder.Services.AddSingleton<IConnectionMultiplexer>(OP =>
+            {
+                var configuration = builder.Configuration.GetConnectionString("Redis");
+                return ConnectionMultiplexer.Connect(configuration!);
+            });
+
+            // Repositories
+            builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+
+            // Services
+            builder.Services.AddScoped<ICacheService, RedisCacheService>();
+            builder.Services.AddScoped<IOrderService, OrderService>();
+
+            // Validators
+            builder.Services.AddValidatorsFromAssemblyContaining<CreateOrderValidator>();
+
+
+
             var app = builder.Build();
 
+
             // Configure the HTTP request pipeline.
+            app.UseMiddleware<ExceptionHandlingMiddleware>();
+
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
